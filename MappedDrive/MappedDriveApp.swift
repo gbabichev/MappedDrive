2b75a0d0403ca0ecfc21224b5eb6886db0bcec5a
George Babichev <gbabichev@xantrion.com>
2026-02-12 13:33:43 -0500

    Initial Commit

---
+++ MappedDrive/MappedDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappedDriveApp.swift
+//  MappedDrive
+//
+//  Created by George Babichev on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MappedDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
